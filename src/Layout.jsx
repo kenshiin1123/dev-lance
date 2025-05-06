f0ad0d0ebf3ed9650d26e1963632cd470c6b5691
kenshiin1123 <128129648+kenshiin1123@users.noreply.github.com>
2025-05-06 17:19:02 +0800

    Added Vercel Web Analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
+import { Analytics } from "@vercel/analytics/react";
 export default function Layout() {
   return (
     <>
       {/* <NavigationBar /> */}
+      <Analytics />
       <Outlet />
     </>
   );

d858b743091bceb0497644fd77318b35af0f52ff
Lance Ivan Gil Fernandez <fernandezlanceivangil@gmail.com>
2025-05-30 23:21:46 +0800

    add clickable project image to redirect user

--- src/components/Projects/Square.jsx
+++ src/components/Projects/Square.jsx
@@ -4,9 +4,12 @@ export default function Square({ name, image, link }) {
   return (
     <div className="size-80 bg-black p-5 flex flex-col justify-center items-center border border-white gap-3 min-sm:size-[40vw] min-md:size-[28vw]">
       <img
+        onClick={() => {
+          window.open(link, "_blank", "noopener,noreferrer");
+        }}
         src={image}
         alt={`${name}-image`}
-        className="aspect-square w-[90%] rounded-lg"
+        className="aspect-square w-[90%] rounded-lg active:scale-95 transition hover:-translate-y-1 hover:shadow-md hover:shadow-blue-600/30"
       />
       <a
         className="text-xl text-white select-none hover:underline"
